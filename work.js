27cb31d1de40014741d87c7f4a01a091a511aa2e
jackbolton901 <bltnjck@gmail.com>
2019-08-07 11:59:34 -0500

    Class work 08/7/2019

---
+++ work.js
@@ -0,0 +1,17 @@
+var myName=("My name is: ");
+var myName2= ("Jack");
+alert(myName + myName2);
+
+var balance= parseInt(prompt("What is your balance?"));
+var nbalance= balance-10;
+alert("You lost $10. Your new balance is "+ nbalance);
+
+var numgrade= parseInt(prompt("Enter your number grade."));
+var extraCred= parseInt(prompt("Enter your extra credit"));
+var endgrade= numgrade +extraCred;
+alert (endgrade);
+
+var balance= parseInt(prompt("What is your balance?"));
+var deduction= parseInt(prompt("How much do you want to deduct"))
+var rebalance= balance- deduction
+alert("Your remaining balance is "+ rebalance + ".");
